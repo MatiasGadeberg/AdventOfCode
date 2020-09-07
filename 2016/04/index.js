f014e3fd8b93f5a39d912c33bd8cf03e2d4dd628
HMGD (Matias Schaff Gadeberg) <hmgd@novonordisk.com>
2020-09-07 22:00:02 +0200

    Started problem 6

--- 2016/04/index.js
+++
@@ -1,7 +0,0 @@
-const RoomFinder = require('./RoomFinder')
-const roomList = require('./input').roomList;
-
-roomFinder = new RoomFinder;
-
-console.log(roomFinder.sumRealRooms(roomList));
-console.log(roomFinder.findRoomFromDescription(roomList, 'northpole object storage'));
--- 2016/06/CommunicationFixer.js
+++ 2016/06/CommunicationFixer.js
@@ -2,6 +2,8 @@ class CommunicationFixer {
     constructor() {
 
     }
+
+    
 }
 
 module.exports = CommunicationFixer;
